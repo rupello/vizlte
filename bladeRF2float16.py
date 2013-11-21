c32ced857ff5792f5a98e85e071b3ba3944c16be
Rupert Lloyd <rupert.lloyd@gmail.com>
2013-11-20 22:09:35 -0500

    add conversions

---
+++ bladeRF2float16.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import numpy
+
+if __name__ == "__main__":
+    filepath = sys.argv[1]
+    assert os.path.exists(filepath)
+    with open(filepath,'rb') as fd:
+        int16data = numpy.fromfile(fd,'int16')
+        float16data = int16data.astype('float16')/2048.
+        float16data.tofile(filepath+'.float16')
